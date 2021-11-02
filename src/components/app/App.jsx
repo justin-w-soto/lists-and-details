2d07c58d558d622542f5d824605c0cc1dd712ca6
Justin W Soto <soto@pdx.edu>
2021-11-02 15:43:30 -0700

    added browser router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AnimalCrossingContainer from '../../containers/AnimalCrossingContainer';
 
 export default function App() {
-  return <AnimalCrossingContainer />;
+
+
+  return (
+    <section className='app'>
+
+    <BrowserRouter>
+    
+      <Switch>
+
+          <Route exact path="/" component={AnimalCrossingContainer}/>
+
+      </Switch>
+      
+    
+    </BrowserRouter>
+
+
+
+    </section>
+  )
+  
 }
